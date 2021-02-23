fce0f4f2b82b28c866c8f4f381ab5c3d8a6c0111
Daniel Madureira <danielmadsv@gmail.com>
2021-02-23 16:22:28 -0300

    Minor corrections
    - Updates docker-composer.yml, removing unused services;
    - Adds default user to user's migration;
    - Removes web routes.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 /**
@@ -19,14 +20,24 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('profession');
+            $table->string('profession')->nullable();;
             $table->string('profile_picture')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('user_type')->default('REGULAR');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
+
+        $domain = parse_url(env('APP_URL', 'localhost'))['host'];
+
+        DB::table('users')->insert([
+            'name' => "Administrator",
+            'email' => "administrator@bys-app.com",
+            'password' => "$2y$10\$Y.6cUP9k1AzFXjYzNxuZ/OyZmp2Hn.jLnRCUgdNW84VN.xEPZeJs.",
+            'user_type' => 'ADMIN'
+        ]);
     }
 
     /**
