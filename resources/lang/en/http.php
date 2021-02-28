cae6e68cce0084f384230f17a2163d649d3d3372
Daniel Madureira <danielmadsv@gmail.com>
2021-02-28 19:43:05 -0300

    Adds error handling locale config
    Adds pt-BR messages and timezone for error responses.

---
+++ resources/lang/en/http.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Exception Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during exception handling for
+    | various messages that we need to display to the user. You are free to
+    | modify these language lines according to your application's
+    | requirements.
+    |
+    */
+
+    'not_found' => 'Not found.',
+    'unknown_error' => 'Unknown server error.',
+    'unprocessable_entity' => 'Unprocessable Entity',
+
+];
---
+++ resources/lang/pt_BR/http.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Exception Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during exception handling for
+    | various messages that we need to display to the user. You are free to
+    | modify these language lines according to your application's
+    | requirements.
+    |
+    */
+
+    'not_found' => 'Não encontrado.',
+    'unknown_error' => 'Erro desconhecido no servidor. Consulte o administrador do sistema.',
+    'unprocessable_entity' => 'Entidade não processável',
+
+];
